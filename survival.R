714f7dd55ee47074c808b8d4408336dd53e26441
rafael747cardoso <rafael747cardoso@gmail.com>
2022-06-26 19:17:03 -0300

    structure

---
+++ survival.R
@@ -0,0 +1,43 @@
+
+############################################## Titanic Survival ##################################################
+
+require(ggplot2)
+require(gridExtra)
+require(grid)
+require(plotly)
+require(dplyr)
+require(tidyr)
+require(fastDummies)
+require(boot)
+require(glmnet)
+require(Amelia)
+
+
+############ Model selection
+
+###### Forward Stepwise Selection - Logistic Regression
+
+###### Ridge - Logistic Regression
+
+###### Lasso - Logistic Regression
+
+###### PCA - Logistic Regression
+
+###### Regularized Discriminant Analysis
+
+###### k-Nearest Neighbours
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
